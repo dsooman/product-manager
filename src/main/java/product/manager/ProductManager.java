6239bf0a3e0e59cb3aa378292671daf8fad1fc8d
user <user@server>
2020-01-24 16:50:30 +0000

    add basic spring boot java 11 project

---
+++ src/main/java/product/manager/ProductManager.java
@@ -0,0 +1,12 @@
+package product.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManager {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ProductManager.class, args);
+	}
+}
