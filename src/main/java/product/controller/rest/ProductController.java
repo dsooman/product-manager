0fc56411b70d3851253ff44748a2258a380b556f
user <user@server>
2020-01-26 13:33:47 +0000

    code tidy

--- src/main/java/product/controller/rest/ProductController.java
+++ src/main/java/product/controller/rest/ProductController.java
@@ -32,100 +32,101 @@ public class ProductController {
 
 	/**
 	 * Block a Product.
-	 * 
+	 *
 	 * @param name - name of the Product to block.
 	 */
 	@PatchMapping("/block/{name}")
-	public void block(@PathVariable(value = "name") String name) {
-		productService.block(name);
+	public void block(@PathVariable(value = "name") final String name) {
+		this.productService.block(name);
 	}
 
 	/**
 	 * Create a Product.
-	 * 
+	 *
 	 * @param name- name of the Product to create.
 	 * @return Product that was created
 	 */
 	@PostMapping("/create")
-	public Product create(@RequestBody String name) {
-		return productService.create(name);
+	public Product create(@RequestBody final String name) {
+		return this.productService.create(name);
 	}
 
 	/**
 	 * Delete a Product.
-	 * 
+	 *
 	 * @param name - name of the Product to be deleted.
 	 */
 	@DeleteMapping
 	public void delete(@RequestBody final String name) {
-		productService.delete(name);
+		this.productService.delete(name);
 	}
 
 	/**
 	 * Gets the Last generated Product purchase requirements. Will return 404 if
 	 * /productPurchaseRequirements has never been ran.
-	 * 
+	 *
 	 * @return Set of Product purchase requirements
 	 */
 	@GetMapping("/latestProductPurchaseRequirements")
 	public Set<ProductPurchaseRequirement> getLatestProductPurchaseRequirements() {
-		return productService.getLatestProductPurchaseRequirements();
+		return this.productService.getLatestProductPurchaseRequirements();
 	}
 
 	/**
 	 * Generates and gets Product purchase requirements, based on rules that have
 	 * been defined for Products.
-	 * 
+	 *
 	 * @return Set of Product purchase requirements
 	 */
 	@GetMapping("/productPurchaseRequirements")
 	public Set<ProductPurchaseRequirement> getProductPurchaseRequirements() {
-		return productService.getProductPurchaseRequirements();
+		return this.productService.getProductPurchaseRequirements();
 	}
 
 	/**
 	 * Overrides a Product amount to purchase, regardless if the Product is blocked
 	 * or has a different minimum stock level already set.
-	 * 
+	 *
 	 * @param name   - name of the Product
 	 * @param amount - amount of the Product that is required
 	 */
 	@PatchMapping("/override/{name}/{amount}")
-	public void overrideMinAmount(@PathVariable(value = "name") String name,
-			@PathVariable(value = "amount") Long amount) {
-		productService.overrideMinAmount(name, amount);
+	public void overrideMinAmount(@PathVariable(value = "name") final String name,
+			@PathVariable(value = "amount") final Long amount) {
+		this.productService.overrideMinAmount(name, amount);
 	}
 
 	/**
 	 * Sets the current amount of a Product in stock.
-	 * 
+	 *
 	 * @param name   - name of the Product
 	 * @param amount - amount of the Product in stock
 	 */
 	@PatchMapping("/current/{name}/{amount}")
-	public void setCurrentAmount(@PathVariable(value = "name") String name,
-			@PathVariable(value = "amount") Long amount) {
-		productService.setCurrentAmount(name, amount);
+	public void setCurrentAmount(@PathVariable(value = "name") final String name,
+			@PathVariable(value = "amount") final Long amount) {
+		this.productService.setCurrentAmount(name, amount);
 	}
 
 	/**
 	 * Sets the minimum required amount of the Product to have in stock.
-	 * 
+	 *
 	 * @param name   - name of the Product
 	 * @param amount - amount of the Product required to be in stock
 	 */
 	@PatchMapping("/min/{name}/{amount}")
-	public void setMinAmount(@PathVariable(value = "name") String name, @PathVariable(value = "amount") Long amount) {
-		productService.setMinAmount(name, amount);
+	public void setMinAmount(@PathVariable(value = "name") final String name,
+			@PathVariable(value = "amount") final Long amount) {
+		this.productService.setMinAmount(name, amount);
 	}
 
 	/**
 	 * Unblock a Product.
-	 * 
+	 *
 	 * @param name - name of the Product to unblock.
 	 */
 	@PatchMapping("/unblock/{name}")
-	public void unblock(@PathVariable(value = "name") String name) {
-		productService.unblock(name);
+	public void unblock(@PathVariable(value = "name") final String name) {
+		this.productService.unblock(name);
 	}
 }
